f49ea5bbe25a23e6a28a3b83177a5b1df7a2b058
Jerry Young <jerryyoung2911@gmail.com>
2023-03-16 10:20:18 +0700

    feature: edit barang

--- produk/model.go
+++ produk/model.go
@@ -59,3 +59,27 @@ func (pm ProdukModel) GetAllProduct() ([]Produk, error) {
 
 	return listProduk, nil
 }
+
+func (pm *ProdukModel) EditBarang(barcode int, updatedData Produk) error {
+	res, err := pm.conn.Exec("UPDATE produk SET nama = ?, harga = ?, qty = ?", barcode, updatedData.Nama, updatedData.Harga, updatedData.Qty)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	aff, err := res.RowsAffected()
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if aff <= 0 {
+		return errors.New("terjadi sebuah masalah pada sistem")
+	}
+
+	pm.conn.Close()
+
+	return nil
+}
